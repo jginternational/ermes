7e55f4df733c4b6945099f93648a4e06fd856675
Javi Gárate <fjgarate@cimne.upc.edu>
2026-07-30 16:01:27 +0200

    open tree

--- ermesv2.gid/scripts/tree.tcl
+++ ermesv2.gid/scripts/tree.tcl
@@ -1,35 +1,20 @@
 proc ::Ermes::TreeOpenBranch { branchName } {
     # close all the customlib tree branches
-    set document [$::gid_groups_conds::doc documentElement]
+    set root [customlib::GetBaseRoot]
     set xpath {//container} 
     set node_to_open ""
-    foreach branch [$document selectNodes $xpath] {
+    foreach branch [$root selectNodes $xpath] {
         if { [string equal [$branch getAttribute n] $branchName] } {
-            W "Set to open [$branch @n]"
             set node_to_open $branch
         } else {
-            $branch setAttribute tree_state close
-            W "Closing [$branch @n]"
+            $branch setAttribute tree_state "close"
         }
     }
     if { [string equal $node_to_open ""] } {
-        W "Branch [$branchName] not found"
         return
     }
+    gid_groups_conds::open_conditions show_window -select_xpath [$node_to_open toXPath]
     
-    
-    # open all the containers to reach the node to open
-    while { ! [string equal $node_to_open ""] } {
-        $node_to_open setAttribute tree_state open
-        if {[$node_to_open hasAttribute n]} {
-            
-            W "Opening [$node_to_open @n]"
-            set node_to_open [$node_to_open parentNode]
-        } else {
-            set node_to_open ""
-        }
-    }
-    W "Opened all path"
     
     gid_groups_conds::actualize_conditions_window
 }
